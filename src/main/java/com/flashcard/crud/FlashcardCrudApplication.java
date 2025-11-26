6ae8e8dfe5426fc3fb0b8fad11f9dea9f690d998
RonanOlmos <78549999+RonanOlmos@users.noreply.github.com>
2025-11-25 22:18:40 -0300

    Inicializar Spring Boot con Maven y configuraciones basicas

---
+++ src/main/java/com/flashcard/crud/FlashcardCrudApplication.java
@@ -0,0 +1,13 @@
+package com.flashcard.crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlashcardCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlashcardCrudApplication.class, args);
+	}
+
+}
